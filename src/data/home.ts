128b4dd2005c9898822dd14e8b30d748a9ed7f8e
VIPUL070 <sutharvipul007@gmail.com>
2026-06-17 19:09:02 +0530

    Adding data of navbar and footer

---
+++ src/data/home.ts
@@ -0,0 +1,9 @@
+export const NAV_ITEMS = ["Home", "Bookings", "About Us" , "Contact"]
+
+export const FOOTER_LINKS = [
+  { name: "Privacy policy", href: "/privacy-policy" },
+  { name: "Refund policy", href: "/refund-policy" },
+  { name: "Terms of service", href: "/terms-of-service" },
+  { name: "Contact information", href: "/contact" },
+  { name: "Shipping policy", href: "/shipping-policy" },
+];
