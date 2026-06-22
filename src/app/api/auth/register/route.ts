84cc8b26d271a1d5f13b118efa7dcdb63513f6f0
VIPUL070 <sutharvipul007@gmail.com>
2026-06-22 17:17:58 +0530

    Creating functionality to send and verify otp

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,6 +3,7 @@ import User from "@/models/User.model";
 import bcrypt from "bcryptjs";
 import z from "zod";
 import { NextRequest, NextResponse } from "next/server";
+import { sendMail } from "@/lib/sendMail";
 
 export const RegisterSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters").max(50),
@@ -10,15 +11,17 @@ export const RegisterSchema = z.object({
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
+const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
+
 export async function POST(req: NextRequest) {
   try {
-    const body= await req.json();
+    const body = await req.json();
 
     const validation = RegisterSchema.safeParse(body);
     if (!validation.success) {
       return NextResponse.json(
-        { 
-          message: "Validation failed", 
+        {
+          message: "Validation failed",
           errors: validation.error
         },
         { status: 400 }
@@ -26,18 +29,10 @@ export async function POST(req: NextRequest) {
     }
 
     const { name, email, password } = validation.data;
-
-    if (!name || !email || !password) {
-      return NextResponse.json(
-        { message: "All fields (name, email, password) are required" },
-        { status: 400 }
-      );
-    }
-
     await connectDB();
 
     const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    if (existingUser && existingUser.isEmailVerified) {
       return NextResponse.json(
         { message: "User already exists with this email" },
         { status: 400 }
@@ -45,30 +40,61 @@ export async function POST(req: NextRequest) {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+    const otp = generateOTP();
+    const otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
+    const emailTemplate = `
+      <div style="font-family: DM-sans, sans-serif; max-width: 600px; margin: 0 auto;">
+        <h2>Verify Your Email</h2>
+        <p>Thank you for registering. Please use the following One-Time Password (OTP) to complete your verification. This code is valid for 5 minutes.</p>
+        <h1 style="background: #f4f4f4; padding: 10px; text-align: center; letter-spacing: 5px; color: #333;">${otp}</h1>
+        <p>If you did not request this code, please ignore this email.</p>
+      </div>
+    `;
 
-    const newUser = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+    if (existingUser && !existingUser.isEmailVerified) {
+      existingUser.name = name;
+      existingUser.email = email;
+      existingUser.password = hashedPassword;
+      existingUser.otp = otp;
+      existingUser.otpExpiry = otpExpiry
+      await existingUser.save();
 
-    return NextResponse.json(
-      {
-        message: "User registered successfully",
-        user: {
-          id: newUser._id,
-          name: newUser.name,
-          email: newUser.email,
-          createdAt: newUser.createdAt,
-          updatedAt: newUser.updatedAt
+      await sendMail(email, "Your OTP for Email Verification", emailTemplate);
+
+      return NextResponse.json(
+        { message: "Account pending verification. New OTP sent." },
+        { status: 200 }
+      );
+
+    } else {
+      const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        otp,
+        otpExpiry
+      });
+
+      await sendMail(email, "Your OTP for Email Verification", emailTemplate);
+
+      return NextResponse.json(
+        {
+          message: "User registered successfully. Please verify your email.",
+          user: {
+            id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            createdAt: newUser.createdAt,
+            updatedAt: newUser.updatedAt
+          },
         },
-      },
-      { status: 201 }
-    );
+        { status: 201 }
+      );
+    }
 
   } catch (error) {
     console.error("Registration Error:", error);
-    
+
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
--- src/components/AuthForms/OtpForm.tsx
+++ src/components/AuthForms/OtpForm.tsx
@@ -1,13 +1,22 @@
 "use client";
-import { useState, useRef } from "react";
-import { motion } from "motion/react";
+import { useState, useRef, SyntheticEvent } from "react";
+import { AnimatePresence, motion } from "motion/react";
 import { ArrowRight } from "lucide-react";
 import { stepContent } from "@/data/authForm";
 import Button from "../ui/Button";
+import axios from "axios";
+import Spinner from "../ui/Spinner";
+import FormError from "../ui/FormError";
 
-function OtpForm() {
-  const [otp, setOtp] = useState<string[]>(new Array(4).fill(""));
+interface OTPProps {
+    onStepChange: () => void;
+    email: string;
+}
+function OtpForm({email, onStepChange}: OTPProps) {
+  const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
   const otpRefs = useRef<HTMLInputElement[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const handleOtpChange = (value: string, index: number) => {
     if (isNaN(Number(value))) return;
@@ -15,38 +24,56 @@ function OtpForm() {
     newOtp[index] = value.substring(value.length - 1);
     setOtp(newOtp);
 
-    if (value && index < 3) {
+    if (value && index < 5) {
       otpRefs.current[index + 1]?.focus();
     }
   };
 
-  const handleOtpKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+  const handleOtpKeyDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
     if (e.key === "Backspace" && !otp[index] && index > 0) {
       otpRefs.current[index - 1]?.focus();
     }
   };
 
-//   const clearOtpFields = () => {
-//     setOtp(new Array(4).fill(""));
-//     onClearOtp();
-//   };
-
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleVerifyOTP = async (e: SyntheticEvent) => {
     e.preventDefault();
+    setLoading(true);
+    setError("");
+    try {
+      const { data } = await axios.post("/api/auth/verify-otp", {
+        email,
+        otp: otp.join(""),
+      });
+      onStepChange();
+      setOtp(new Array(6).fill(""));
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data?.message ?? "Something went wrong!");
+      } else if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unexpected error occurred.");
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <div className="flex flex-col">
       <div className="text-center mb-6">
         <h1 className="text-[2.25rem] font-extrabold tracking-tight uppercase text-secondary">
-          {stepContent['otp'].title}
+          {stepContent["otp"].title}
         </h1>
         <p className="text-[0.9rem] pt-1 text-secondary/60 tracking-tight">
-          {stepContent['otp'].subtitle}
+          {stepContent["otp"].subtitle}
         </p>
       </div>
 
-      <form onSubmit={handleSubmit} className="space-y-3">
+      <form onSubmit={handleVerifyOTP} className="space-y-3">
         <div className="flex flex-col gap-2.5 items-center py-2">
           <label className="text-sm font-medium tracking-wide text-secondary/60 uppercase self-start">
             Security Code
@@ -55,7 +82,9 @@ function OtpForm() {
             {otp.map((digit, index) => (
               <input
                 key={index}
-                ref={(el) => { otpRefs.current[index] = el!; }}
+                ref={(el) => {
+                  otpRefs.current[index] = el!;
+                }}
                 type="text"
                 maxLength={1}
                 value={digit}
@@ -69,18 +98,29 @@ function OtpForm() {
           </div>
         </div>
 
+        <AnimatePresence mode="wait">
+          {error && <FormError message={error} />}
+        </AnimatePresence>
+
         <Button
           whileHover="hover"
           whileTap={{ scale: 0.98 }}
+          type="submit"
           className="group flex w-full items-center justify-center gap-2 rounded-xl bg-secondary px-4 py-3.5 text-sm font-medium text-background shadow-lg transition-colors duration-200 hover:opacity-90 cursor-pointer"
         >
-          {stepContent['otp'].buttonText}
-          <motion.div
-            variants={{ hover: { x: 3 } }}
-            transition={{ type: "spring", stiffness: 400, damping: 15 }}
-          >
-            <ArrowRight className="h-4 w-4" />
-          </motion.div>
+          {loading ? (
+            <Spinner />
+          ) : (
+            <>
+              {stepContent["otp"].buttonText}
+              <motion.div
+                variants={{ hover: { x: 3 } }}
+                transition={{ type: "spring", stiffness: 400, damping: 15 }}
+              >
+                <ArrowRight className="h-4 w-4" />
+              </motion.div>
+            </>
+          )}
         </Button>
       </form>
     </div>
