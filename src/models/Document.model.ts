d693e6c3fd0433fdbfe6e5a87fd8324ed157c5b6
VIPUL070 <sutharvipul007@gmail.com>
2026-06-24 22:20:57 +0530

    Creating vehicle docs and partner bank details model

---
+++ src/models/Document.model.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+interface IDocument{
+    owner: mongoose.Types.ObjectId;
+    aadharUrl: string;
+    rcUrl: string;
+    licenseUrl: string;
+    status: "approved" | "pending" | "rejected";
+    rejectionReason?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const documentSchema = new mongoose.Schema<IDocument>({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    aadharUrl: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    rcUrl: {
+        type: String,
+        required: true,
+    },
+    licenseUrl: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["approved", "pending", "rejected"],
+        default: "pending",
+    },
+    rejectionReason: {
+        type: String,
+        trim: true,
+        default: "",
+    }
+}, { timestamps: true })
+
+const Document = mongoose.models.Document || mongoose.model("Document", documentSchema);
+export default Document;
