cf87b08a151dbfca1e3199129b3e459ea60d5a0c
VIPUL070 <sutharvipul007@gmail.com>
2026-09-09 22:57:19 +0530

    Returned populated data fromt he booking model

--- glide-client/src/app/api/partner/my-active/route.ts
+++ glide-client/src/app/api/partner/my-active/route.ts
@@ -27,7 +27,7 @@ export async function GET() {
         const booking = await Booking.findOne({
             driver: user._id,
             bookingStatus: { $in: ["confirmed", "started", "completed"] },
-        }).populate("user")
+        }).populate("user driver")
 
         if (!booking) {
             return NextResponse.json(
